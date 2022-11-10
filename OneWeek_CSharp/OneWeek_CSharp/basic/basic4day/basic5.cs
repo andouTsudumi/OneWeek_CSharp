959ecbf29dd4ca8a4580b7658f79c0d229712fdf
fca <fca>
2022-11-10 19:10:17 +0900

    基礎四日目

---
+++ OneWeek_CSharp/OneWeek_CSharp/basic/basic4day/basic5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneWeek.basic.basic4day
+{
+    class basic5
+    {
+        static void Main()
+        {
+            int i, num;
+            num = 1;
+
+            Console.Write("whileで実行：");
+            i = 1;
+            while(i < num)
+            {
+                Console.Write("*");
+                i++;
+            }
+            Console.WriteLine();
+
+            Console.Write("do～whileで実行：");
+            i = 1;
+            do
+            {
+                Console.WriteLine("*");
+            } while (i < num);
+        }
+    }
+}
