91a9f22234114c0bbb15f06cb36e8e8160d45d14
fca <fca>
2022-11-10 19:10:47 +0900

    基礎七日目

---
+++ OneWeek_CSharp/OneWeek_CSharp/basic/basic7day/basic7-2/main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneWeek.basic.basic7day.basic7_2
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            Access a = new Access();
+
+            //a.Data1 = 1;
+            a.Data2 = 2;
+            a.ShowDatas();
+            Console.WriteLine("a.data1 = {0}", a.Data1);
+            //Console.WriteLine("a.data2 = {0}", a.Data2);
+        }
+    }
+}
---
+++ OneWeek_CSharp/OneWeek_CSharp/basic/basic7day/basic7-3/main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneWeek.basic.basic7day.basic7_3
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            Person2 p = new Person2();
+
+            p.SetAgeAndName("おちんぽ", 26);
+
+            p.Age = 32;
+            //p.Name = 36;
+
+            Console.WriteLine("名前：{0}、年齢：{1}", p.Name, p.Age);
+        }
+    }
+}
