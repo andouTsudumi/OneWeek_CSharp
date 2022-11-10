44e700c14c46ddff08f292674e6fc0a90981f41f
fca <fca>
2022-11-10 19:10:07 +0900

    基礎三日目

---
+++ OneWeek_CSharp/OneWeek_CSharp/basic/basic3day/basic1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneWeek.basic.basic3day
+{
+    class basic1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("整数値：");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("a = " + a);
+
+            if (a > 0)
+            {
+                Console.WriteLine("aは正の数です。");
+            }
+        }
+    }
+}
---
+++ OneWeek_CSharp/OneWeek_CSharp/basic/basic3day/basic6.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneWeek.basic.basic3day
+{
+    class basic6
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("さいころの目(1～6)：");
+
+            int dice = int.Parse(Console.ReadLine());
+
+            switch (dice)
+            {
+                case 1:
+                case 3:
+                case 5:
+                    Console.WriteLine("丁(チョウ)です。");
+                    break;
+
+                case 2:
+                case 4:
+                case 6:
+                    Console.WriteLine("半(ハン)です。");
+                    break;
+
+                default:
+                    Console.WriteLine("範囲外の数値です。");
+                    break;
+            }
+        }
+    }
+}
