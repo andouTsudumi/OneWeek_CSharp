782a3bfe3f89ee1938a9b7bc143e67cc20a060e6
fca <fca>
2022-11-10 19:09:56 +0900

    基礎二日目

---
+++ OneWeek_CSharp/OneWeek_CSharp/basic/basic2day/basic1.cs
@@ -0,0 +1,28 @@
+﻿
+// 変数なしの演算方法.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneWeek.basic.basic2day
+{
+    class basic1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} + {1} = {2}", 5, 2, 5 + 2); // 足し算
+
+            /*
+             * 今まで%dで入力していたものは{左から何番目なのかの数字}で表記するとのこと.
+             *  +	プラス	      足し算を行う演算子	                 5 + 5
+             *  -	マイナス	  引き算を行う演算子	                 7 - 3
+             *  *	アスタリスク  掛け算を行う演算子	                 7 * 3
+             *  /	スラッシュ	  割り算を行う演算子	                 7 / 3
+             *  %	パーセント	  剰余（じょうよ）演算子。割り算の余り	 7 % 3
+             */
+        }
+    }
+}
---
+++ OneWeek_CSharp/OneWeek_CSharp/basic/basic2day/basic4.cs
@@ -0,0 +1,27 @@
+﻿
+// キャストとデータの型変換を使用したコンソールへの表示方法.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneWeek.basic.basic2day
+{
+    class basic4
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            double b, c, d;
+
+            a = (int)1.23;
+            b = 1.23;
+            c = 10;
+            d = (double)c;
+
+            Console.WriteLine("a = {0}, b = {1}, c = {2}, d = {3}", a, b, c, d);
+        }
+    }
+}
