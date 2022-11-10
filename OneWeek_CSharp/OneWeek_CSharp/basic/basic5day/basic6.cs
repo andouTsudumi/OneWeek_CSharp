f893838f21bf17ddb70817b0e9883df23e77ce2d
fca <fca>
2022-11-10 19:10:26 +0900

    基礎五日目

---
+++ OneWeek_CSharp/OneWeek_CSharp/basic/basic5day/basic6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneWeek.basic.basic5day
+{
+    class basic6
+    {
+        static void Main(string[] args)
+        {
+            int[][] a = new int[][] { new int[] { 0, 1 }, new int[] { 2 }, new int[] { 3, 4, 5, 6 } };
+
+            for(int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < a[i].Length; j++)
+                {
+                    Console.Write(a[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
