22f15c4327b555696839bb31dd7e740e09681e2b
AndouTsudumi <55andouTsudumi25@gmail.com>
2022-11-19 23:57:17 +0900

    応用１－３の追加
    ただしうまく動作はしていない

---
+++ OneWeek_CSharp/OneWeek_CSharp/practical/practical1day/1-3/Dummy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneWeek_CSharp.practical.practical1day._1_3
+{
+    class Dummy
+    {
+        // コンストラクタ.
+        public Dummy()
+        {
+            Console.WriteLine("コンストラクタ");
+        }
+
+        // デストラクタ.
+        ~Dummy()
+        {
+            Console.WriteLine("デストラクタ");//@@@
+        }
+    }
+}
---
+++ OneWeek_CSharp/OneWeek_CSharp/practical/practical1day/1-3/main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneWeek_CSharp.practical.practical1day._1_3
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            Dummy d = new Dummy();
+        }
+    }
+}
